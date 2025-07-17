7ee76edff73b40610f4d959e8be43e045619caa0
master-mak <mr.almakar@gmail.com>
2025-07-17 18:51:01 +0500

    Update upload.php

--- html/upload.php
+++ html/upload.php
@@ -1,14 +1,17 @@
 <?php
-\$base_dir = '/var/www/html/meetings/';
-\$folder = basename(\$_POST['folder']);
-\$target_dir = \$base_dir . \$folder . "/";
-if (!is_dir(\$target_dir)) {
-    mkdir(\$target_dir, 0775, true);
+$base_dir = '/var/www/html/meetings/';
+$folder = basename($_POST['folder']);
+$target_dir = $base_dir . $folder . "/";
+
+if (!is_dir($target_dir)) {
+    mkdir($target_dir, 0775, true);
 }
-\$target_file = \$target_dir . basename(\$_FILES["fileToUpload"]["name"]);
-if (move_uploaded_file(\$_FILES["fileToUpload"]["tmp_name"], \$target_file)) {
-    echo "Файл ". htmlspecialchars(basename(\$_FILES["fileToUpload"]["name"])) ." загружен.";
+
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "Файл " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " загружен.";
 } else {
-    echo "Ошибка загрузки.";
+    echo "Ошибка загрузки файла.";
 }
 ?>
