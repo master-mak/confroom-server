5e93abdcd64726b3c18c9d5f8d3345e46f74aac7
master-mak <mr.almakar@gmail.com>
2025-07-21 17:59:55 +0500

    Update stream_hdmi.sh

--- scripts/stream_hdmi.sh
+++ scripts/stream_hdmi.sh
@@ -2,9 +2,9 @@
 
 ffmpeg \
   -f v4l2 -input_format mjpeg -video_size 1280x720 -framerate 5 -i /dev/video2 \
-  -f alsa -i hw:1 \
+ # -f alsa -i hw:1 \
   -vcodec libx264 -preset veryfast -tune stillimage -crf 23 -pix_fmt yuv420p \
-  -acodec aac -ar 44100 -ac 2 \
+ # -acodec aac -ar 44100 -ac 2 \
   -f hls \
   -hls_time 2 \
   -hls_list_size 4 \
